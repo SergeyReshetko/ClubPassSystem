0f6f8153bd3b759b8e27e3271a43864dd44f97fb
ReshetkoSergey <dev.rsergey@gmail.com>
2026-01-22 20:41:58 +0300

    Create git repository

---
+++ src/test/java/com/accesssystem/ClubAccessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubAccessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
